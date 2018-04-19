a2ed96c52ef19653974c3335b49b4732d524e675
Anthony <ALeung@Anthonys-MacBook-Pro.local>
2018-04-19 09:30:33 +0100

    Adding transaction statement function

--- src/main/java/com/qa/controller/TransactionDBRepository.java
+++ src/main/java/com/qa/controller/TransactionDBRepository.java
@@ -4,6 +4,7 @@ import static javax.transaction.Transactional.TxType.REQUIRED;
 import static javax.transaction.Transactional.TxType.SUPPORTS;
 
 import java.util.Collection;
+import java.util.Iterator;
 
 import javax.enterprise.inject.Default;
 import javax.inject.Inject;
@@ -12,6 +13,9 @@ import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 import javax.transaction.Transactional;
 
+import org.apache.log4j.Logger;
+
+import com.qa.domain.Customer;
 import com.qa.domain.Transaction;
 import com.qa.service.repository.TransactionRepository;
 import com.qa.util.JSONUtil;
@@ -25,6 +29,8 @@ public class TransactionDBRepository implements TransactionRepository{
 	
 	@Inject
 	private JSONUtil util;
+	
+	private static final Logger LOGGER = Logger.getLogger(TransactionDBRepository.class);
 
 	@Override
 	public String getAllTransactions(long ACC_ID) {
@@ -35,9 +41,34 @@ public class TransactionDBRepository implements TransactionRepository{
 	
 	@Override
 	public String getAllTransactionStatement(long ACC_ID) {
-		Query query = manager.createQuery("Select a FROM Transaction a where ACC_ID = " + ACC_ID + ";");
+		LOGGER.info("ACC_ID: " + ACC_ID);
+		Query query = manager.createQuery("Select a FROM Transaction a where ACC_ID = " + ACC_ID);
 		Collection<Transaction> transactions = (Collection<Transaction>) query.getResultList();
-		return util.getJSONForObject(transactions);
+		LOGGER.info("Transactions: " + transactions);
+		LOGGER.info("At Transaction DB repo - Get request - getAllTransactionStatement");
+		
+		String stringJson = "";
+		if(transactions.size() != 0) {
+			for(Transaction transaction : transactions) {
+				stringJson = stringJson + "{\"TRANS_ID\": \"" + transaction.getTransactionID() + 
+						"\", \"ACC_ID\": \"" + transaction.getAccount().getId() + 
+						"\", \"TYPE\": \"" + transaction.getType() + 
+						"\", \"DATEOFTRANSACTION\": \"" + transaction.getDateOfTransaction() + 
+						"\", \"AMOUNT\": \"" + transaction.getAmount() + "\"}, ";
+			}
+		}else {
+			return "{\"message\": \"Transaction was unsuccessful\"}";
+		}
+		
+		LOGGER.info("Iterator transaction JSON: " + stringJson);
+		
+		stringJson = stringJson.substring(0, (stringJson.length() - 2));
+		
+		LOGGER.info(stringJson);
+		
+		String stringJsonReturn = "[" + stringJson + "]";
+		return stringJsonReturn;
+			
 	}
 
 //	@Override
